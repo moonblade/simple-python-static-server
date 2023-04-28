d8bef4d18f33bceffd817d1969327abee22d1cc3
mnishamk <mnishamk@commvault.com>
2023-04-28 10:16:00 +0530

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+import http.server
+import socketserver
+
+PORT = 8000
+DIRECTORY = "/static_outputs"
+
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
